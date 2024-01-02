65eecfa94fdd3fc9d648439697759c74a1d09348
tabrez510 <alamtabrez510@gmail.com>
2024-01-02 17:56:25 +0530

    get-request

--- index.js
+++ index.js
@@ -13,4 +13,23 @@ function handleFormSubmit(event) {
     axios.post('https://crudcrud.com/api/a004d8ad7b6a4d528ea1f1e31b685429/bookAppointment', obj)
         .then(res => console.log(res))
         .catch(err => console.error(err));
+    showNewUser(obj);
+}
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('https://crudcrud.com/api/a004d8ad7b6a4d528ea1f1e31b685429/bookAppointment')
+    .then(res => {
+        console.log(res.data);
+        for(let i=0; i<res.data.length; i++){
+            showNewUser(res.data[i]);
+        }
+    })
+    .catch(err => console.error(err));
+})
+function showNewUser(user){
+    document.getElementById('username').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+
+    const parent = document.getElementById('listOfItems');
+    parent.innerHTML += `<li>${user.name} - ${user.email} - ${user.phone}</li>`
 }
